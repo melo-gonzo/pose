ea6924b1c1b7d1ead1762e5d7029a3b62196618a
Carmelo <carmelojsg@gmail.com>
2020-10-18 20:06:35 -0700

    Added function for making video of stick figure centered by hip position in x direction.

--- dataProcessing/keypointAnalysis.py
+++ dataProcessing/keypointAnalysis.py
@@ -1,20 +1,21 @@
 # Huge shoutout to https://github.com/CMU-Perceptual-Computing-Lab/openpose
 # Huge shoutout to https://tinyurl.com/y2vuqgxv
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import json
 import cv2
 import os
-import pandas as pd
 
 # download one of the output folders with json files from google drive
 # put in the path to that data here
-dataPath = 'C:\\Users\\carme\\Desktop\\TheProverbialCode\\StickFigure\\keypointData' \
-           '\\lateral_9_17_2020\\'
+file_name = 'fast'
+dataPath = '/home/carmelo/Documents/pose/keypointData/' + file_name + '/'
 
 column_names = ['x', 'y', 'acc']
 path_to_json = dataPath
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+json_files = sorted(json_files)
 count = 0
 # width, height = get_vid_properties()
 body_keypoints_df = pd.DataFrame()
@@ -75,29 +76,57 @@ def make_video(d):
         for c in connections:
             pt1 = tuple(r[c[0]][:2])
             pt2 = tuple(r[c[1]][:2])
-            print(c)
-            cv2.line(img, pt1, pt2, (0, 255, 0), 1)
-            cv2.imshow('', img)
-            cv2.waitKey(1)
+            cv2.line(img, pt1, pt2, (0, 255, 0), 2)
+        cv2.imshow('', img)
+        cv2.waitKey(1)
         return img
 
     connections = [(17, 15), (15, 0), (0, 16), (16, 18), (0, 1), (1, 2), (2, 3), (3, 4), (1, 5),
                    (5, 6), (6, 7), (1, 8), (8, 9), (9, 10), (10, 11), (11, 24), (11, 22), (22, 23),
                    (8, 12), (12, 13), (13, 14), (14, 21), (14, 19), (19, 20)]
 
-    frame_width, frame_height = 2560, 556
+    frame_width, frame_height = 3840, 850
     print('Hard coded video dims')
     codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    out = cv2.VideoWriter('stick.avi', codec, 10, (frame_width, frame_height))
+    out = cv2.VideoWriter(file_name + '.avi', codec, 10, (frame_width, frame_height))
     img = np.ones((frame_height, frame_width, 3)).astype('uint8')
     for k in range(len(d[:, 0])):
         dd = d[k, :].astype('int')
         frame = draw_lines(dd, connections)
-        print(frame)
         out.write(frame)
 
     out.release()
     cv2.destroyAllWindows()
 
 
-make_video(d)
+def make_centered_video(d):
+    def draw_lines(r, connections):
+        img = np.ones((frame_height, frame_width, 3)).astype('uint8')
+        for c in connections:
+            pt1 = tuple(r[c[0]][:2])
+            pt2 = tuple(r[c[1]][:2])
+            cv2.line(img, pt1, pt2, (0, 255, 0), 2)
+        cv2.imshow('', img)
+        cv2.waitKey(1)
+        return img
+
+    connections = [(17, 15), (15, 0), (0, 16), (16, 18), (0, 1), (1, 2), (2, 3), (3, 4), (1, 5),
+                   (5, 6), (6, 7), (1, 8), (8, 9), (9, 10), (10, 11), (11, 24), (11, 22), (22, 23),
+                   (8, 12), (12, 13), (13, 14), (14, 21), (14, 19), (19, 20)]
+
+    frame_width, frame_height = 600, 850
+    print('Hard coded video dims')
+    codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    out = cv2.VideoWriter(file_name + '_centered.avi', codec, 10, (frame_width, frame_height))
+    img = np.ones((frame_height, frame_width, 3)).astype('uint8')
+    for k in range(len(d[:, 0])):
+        dd = d[k, :].astype('int')
+        dd[:, 0] = dd[:, 0] - dd[8, 0] + 300
+        frame = draw_lines(dd, connections)
+        out.write(frame)
+
+    out.release()
+    cv2.destroyAllWindows()
+
+
+make_centered_video(d)
