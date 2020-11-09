050236b312e8c2d65f9e2414d91efe0f90424f59
melo-gonzo <carmelojsg@gmail.com>
2020-11-08 21:21:58 -0800

    added a few time stamps to output

--- data_processing/angleeye.py
+++ data_processing/angleeye.py
@@ -49,8 +49,8 @@ class FetchEmail():
         text = MIMEText(body_text)
         msg.attach(text)
         pdf = MIMEApplication(documentation)
-        fname = 'Quick Start Guide'
-        pdf.add_header('Content-Disposition', 'attachment', filename=fname)
+        fname = 'Quick Start Guide.pdf'
+        pdf.add_header('Content-Disposition', 'attachment', name=fname, filename=fname)
         msg.attach(pdf)
         image = MIMEImage(attachment, name=os.path.basename(attachment_path))
         msg.attach(image)
@@ -146,39 +146,48 @@ class FetchEmail():
 def do_email_thang():
     from credentials import username, password
     while True:
-        AIBikeFit = FetchEmail(mail_server="imap.gmail.com",
-                               username=username,
-                               password=password)
-        emails = AIBikeFit.fetch_unread_messages()
-        if emails is not None:
-            for email in emails:
-                print('Getting name and email address')
-                AIBikeFit.set_name_and_email(AIBikeFit.parse_email_address(email))
-                print('Saving attachment')
-                att_path = AIBikeFit.save_attachment(email)
-                if att_path is not None:
-                    print('Performing inference')
-                    frame, points, angles = inference(att_path)
-                    y_start = frame.shape[1]
-                    frame = np.hstack((frame, np.zeros((frame.shape[0], 750, 3))))
-                    top_text, angle_text = AIBikeFit.gen_angle_text(angles) if angles is not None else None
-                    angle_text = angle_text.split('\n')
-                    for idx, line in enumerate(angle_text):
-                        line = line.split()
-                        locations = [y_start + 10, y_start + 350, y_start+625]
-                        for ii, l in enumerate(line):
-                            cv2.putText(frame, l, (locations[ii], (idx * 50) + 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                                        (255, 255, 255), 2, lineType=cv2.LINE_AA)
-                    inference_path = att_path.split('.')
-                    inference_path = inference_path[0] + '_inference.' + inference_path[1]
-                    print('Saving inference image')
-                    cv2.imwrite(inference_path, frame)
-                    print('Sending email to user')
-                    AIBikeFit.send_email(top_text, inference_path)
-                    print('Sent to: ' + str(AIBikeFit.parse_email_address(email)))
-        print('Sleeping. '+ time.strftime("%Y-%m-%dT%H%M%S", time.localtime()), end='\r')
+        try:
+            AIBikeFit = FetchEmail(mail_server="imap.gmail.com",
+                                   username=username,
+                                   password=password)
+            emails = AIBikeFit.fetch_unread_messages()
+            if emails is not None:
+                for email in emails:
+                    print('Getting name and email address')
+                    AIBikeFit.set_name_and_email(AIBikeFit.parse_email_address(email))
+                    print('Saving attachment')
+                    att_path = AIBikeFit.save_attachment(email)
+                    if att_path is not None:
+                        print('Performing inference')
+                        frame, points, angles = inference(att_path)
+                        y_start = frame.shape[1]
+                        frame = np.hstack((frame, np.zeros((frame.shape[0], 750, 3))))
+                        top_text, angle_text = AIBikeFit.gen_angle_text(angles) if angles is not None else None
+                        angle_text = angle_text.split('\n')
+                        for idx, line in enumerate(angle_text):
+                            line = line.split()
+                            locations = [y_start + 10, y_start + 350, y_start + 625]
+                            for ii, l in enumerate(line):
+                                cv2.putText(frame, l, (locations[ii], (idx * 50) + 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                            (255, 255, 255), 2, lineType=cv2.LINE_AA)
+                        inference_path = att_path.split('.')
+                        inference_path = inference_path[0] + '_inference.' + inference_path[1]
+                        print('Saving inference image')
+                        cv2.imwrite(inference_path, frame)
+                        print('Sending email to user')
+                        AIBikeFit.send_email(top_text, inference_path)
+                        print('Sent to: ' + str(AIBikeFit.parse_email_address(email)) + ' at time: '
+                              + time.strftime("%Y-%m-%dT%H%M%S", time.localtime()))
+        except Exception:
+            print('Problem! ' + time.strftime("%Y-%m-%dT%H%M%S", time.localtime()))
+            pass
+        print('Sleeping. ' + time.strftime("%Y-%m-%dT%H%M%S", time.localtime()), end='\r')
         time.sleep(59)
-        AIBikeFit.close_connection()
+        try:
+            AIBikeFit.close_connection()
+        except Exception:
+            print('Problem! ' + time.strftime("%Y-%m-%dT%H%M%S", time.localtime()))
+            pass
         time.sleep(1)
     # frame, points = inference(media_path)
 
